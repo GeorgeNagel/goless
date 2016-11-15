2d2895780a067f52462e8207fff6e05e3b3f1942
George Nagel <gnagel@datto.com>
2016-11-15 13:06:12 -0500

    Go club episode 2

--- main.go
+++ main.go
@@ -3,25 +3,60 @@ package main
 import (
 	"fmt"
 	"github.com/garyburd/redigo/redis"
-	// "github.com/kdar/goqless"
+	"io/ioutil"
 )
 
 func main() {
+
 	conn, err := redis.Dial("tcp", fmt.Sprintf("%s:%s", "localhost", "6380"))
 	if err != nil {
 		panic(err)
 	}
 
-	// Get job ids in queue
-	jobs, err := redis.Strings(conn.Do("ZRANGE", "ql:q:audit_events-work", "0", "100"))
-	fmt.Printf("Jobs: %s\n", jobs)
-
-	for _, j := range jobs {
-		err = printJob(conn, j)
-		if err != nil {
-			panic(err)
-		}
+	script, err := readLuaScript()
+	if err != nil {
+		panic(err)
 	}
+
+	sha, err := loadLuaScript(script, conn)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(sha)
+
+	result, err := popJob(conn, "update_google_accounts", "test-worker", sha)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result)
+
+	// // Get job ids in queue
+	// jobs, err := redis.Strings(conn.Do("ZRANGE", "ql:q:audit_events-work", "0", "100"))
+	// fmt.Printf("Jobs: %s\n", jobs)
+
+	// for _, j := range jobs {
+	// 	err = printJob(conn, j)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// }
+
+}
+
+func popJob(conn redis.Conn, queue string, worker string, scriptSha string) (string, error) {
+	result, err := redis.String(conn.Do("EVALSHA", scriptSha, "pop", queue, worker, 1))
+	return result, err
+}
+
+func loadLuaScript(script string, conn redis.Conn) (string, error) {
+	sha, err := redis.String(conn.Do("SCRIPT", "LOAD", script))
+	return sha, err
+}
+
+func readLuaScript() (string, error) {
+	bytes, err := ioutil.ReadFile("./qless.lua")
+	return string(bytes), err
 }
 
 func printJob(conn redis.Conn, jobId string) error {
