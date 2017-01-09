5e989aaebadcdf9dab571b9bf9d7a2ac87aeee79
George Nagel <gnagel@datto.com>
2017-01-09 10:00:20 -0500

    PopJob returns Job. Run and Heartbeat now live on Job

--- main.go
+++ main.go
@@ -3,63 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
-	"strings"
 	"time"
 
 	"github.com/GeorgeNagel/goless/qconn"
 )
-
-func Heartbeat(connPool *qconn.QPool, jobId string, dataString string, beatPeriod int, stopHeartbeat chan string) {
-	for {
-		time.Sleep(time.Duration(beatPeriod) * time.Second)
-		select {
-		case _ = <-stopHeartbeat:
-			// We have received a message that the job is done and we can stop heart-beating
-			return
-		default:
-			fmt.Printf("[%s] Heartbeating\n", jobId)
-			_, err := connPool.Heartbeat(jobId, dataString)
-			if err != nil {
-				errMessage := err.Error()
-
-				// "Job does not exist" corresponds to a job canceled in Qless,
-				// which qless returns as an error to the heartbeat check-in
-				if !strings.Contains(errMessage, "Job does not exist") {
-					fmt.Printf("[%s] Unexpected heartbeat error: %s\n", jobId, errMessage)
-				}
-				stopHeartbeat <- "Bad heartbeat/Job canceled"
-				return
-			}
-		}
-	}
-}
-
-func RunJob(connPool *qconn.QPool, jobId string, dataString string) {
-	stopHeartbeat := make(chan string)
-	go Heartbeat(connPool, jobId, dataString, 5, stopHeartbeat)
-
-	// pretend to do actual work
-	for i := 0; i < 10; i++ {
-		select {
-		case _ = <-stopHeartbeat:
-			fmt.Printf("[%s]Heart stopped. Killing job.\n", jobId)
-			return
-		default:
-			fmt.Printf("[%s] Doing a unit of work\n", jobId)
-			time.Sleep(2 * time.Second)
-		}
-	}
-
-	// Finish the Job
-	// Stop heartbeater before telling qless server that we're done
-	// in order to avoid heartbeating for a completed job
-	stopHeartbeat <- "Done!"
-	result, err := connPool.CompleteJob(jobId, dataString)
-	if err != nil {
-		fmt.Printf("[%s] Bad complete: %s\n", jobId, err)
-	}
-	fmt.Printf("[%s] %s\n", jobId, result)
-}
 
 func main() {
 	connPool, err := qconn.NewQPool("localhost", "6380", "test_queue", "test-worker")
@@ -70,28 +17,17 @@ func main() {
 
 	for {
 		// Get job params
-		jobMap, err := connPool.PopJob()
+		job, err := connPool.PopJob()
 		if err != nil {
 			log.Fatal(err)
 		}
-		if jobMap == nil {
+		if job == nil {
 			fmt.Println("[manager] No jobs on the queue")
 			time.Sleep(10 * time.Second)
 			continue
 		}
-		fmt.Printf("[manager] About to run: %s\n", jobMap)
+		fmt.Printf("[manager] About to run: %s\n", job)
 
-		jobId, ok := jobMap["jid"].(string)
-		if !ok {
-			log.Fatalf("Job ID \"%v\"is not a string!\n", jobMap["jid"])
-		}
-
-		data := jobMap["data"]
-		dataString, ok := data.(string)
-		if !ok {
-			log.Fatal("Job data not a string!")
-		}
-
-		go RunJob(connPool, jobId, dataString)
+		go job.Run(connPool)
 	}
 }
--- qconn/qconn.go
+++ qconn/qconn.go
@@ -6,6 +6,8 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"strings"
 	"time"
 
 	"github.com/garyburd/redigo/redis"
@@ -28,6 +30,63 @@ type QPool struct {
 	WorkerName string
 }
 
+type Job struct {
+	id   string
+	data string
+}
+
+func (job *Job) Run(connPool *QPool) {
+	stopHeartbeat := make(chan string)
+	go job.Heartbeat(connPool, 5, stopHeartbeat)
+
+	// pretend to do actual work
+	for i := 0; i < 10; i++ {
+		select {
+		case _ = <-stopHeartbeat:
+			fmt.Printf("[%s]Heart stopped. Killing job.\n", job.id)
+			return
+		default:
+			fmt.Printf("[%s] Doing a unit of work\n", job.id)
+			time.Sleep(2 * time.Second)
+		}
+	}
+
+	// Finish the Job
+	// Stop heartbeater before telling qless server that we're done
+	// in order to avoid heartbeating for a completed job
+	stopHeartbeat <- "Done!"
+	result, err := connPool.CompleteJob(job)
+	if err != nil {
+		fmt.Printf("[%s] Bad complete: %s\n", job.id, err)
+	}
+	fmt.Printf("[%s] %s\n", job.id, result)
+}
+
+func (job *Job) Heartbeat(connPool *QPool, beatPeriod int, stopHeartbeat chan string) {
+	for {
+		time.Sleep(time.Duration(beatPeriod) * time.Second)
+		select {
+		case _ = <-stopHeartbeat:
+			// We have received a message that the job is done and we can stop heart-beating
+			return
+		default:
+			fmt.Printf("[%s] Heartbeating\n", job.id)
+			_, err := connPool.Heartbeat(job)
+			if err != nil {
+				errMessage := err.Error()
+
+				// "Job does not exist" corresponds to a job canceled in Qless,
+				// which qless returns as an error to the heartbeat check-in
+				if !strings.Contains(errMessage, "Job does not exist") {
+					fmt.Printf("[%s] Unexpected heartbeat error: %s\n", job.id, errMessage)
+				}
+				stopHeartbeat <- "Bad heartbeat/Job canceled"
+				return
+			}
+		}
+	}
+}
+
 func NewQPool(hostname string, port string, queue string, workerName string) (*QPool, error) {
 	pool := newRedisPool(fmt.Sprintf("%s:%s", hostname, port))
 	conn := pool.Get()
@@ -45,7 +104,7 @@ func NewQPool(hostname string, port string, queue string, workerName string) (*Q
 	return &QPool{Pool: pool, Queue: queue, ScriptSha: sha, WorkerName: workerName}, nil
 }
 
-func (pool *QPool) PopJob() (map[string]interface{}, error) {
+func (pool *QPool) PopJob() (*Job, error) {
 	conn := pool.Pool.Get()
 	defer conn.Close()
 
@@ -68,36 +127,48 @@ func (pool *QPool) PopJob() (map[string]interface{}, error) {
 		err = errors.New("Could not cast to interface")
 		return nil, err
 	}
-	return jobMap, err
+
+	jobId, ok := jobMap["jid"].(string)
+	if !ok {
+		log.Fatalf("Job ID \"%v\"is not a string!\n", jobMap["jid"])
+	}
+
+	data := jobMap["data"]
+	dataString, ok := data.(string)
+	if !ok {
+		log.Fatal("Job data not a string!")
+	}
+	var job Job = Job{id: jobId, data: dataString}
+	return &job, err
 }
 
-func (connPool *QPool) Heartbeat(jobId string, data string) (int64, error) {
+func (connPool *QPool) Heartbeat(job *Job) (int64, error) {
 	conn := connPool.Pool.Get()
 	defer conn.Close()
 
 	now := time.Now()
 	seconds := now.Unix()
-	result, err := redis.Int64(conn.Do("EVALSHA", connPool.ScriptSha, 0, "heartbeat", seconds, jobId, connPool.WorkerName, data))
+	result, err := redis.Int64(conn.Do("EVALSHA", connPool.ScriptSha, 0, "heartbeat", seconds, job.id, connPool.WorkerName, job.data))
 	return result, err
 }
 
-func (connPool *QPool) CompleteJob(jobId string, data string) (string, error) {
+func (connPool *QPool) CompleteJob(job *Job) (string, error) {
 	conn := connPool.Pool.Get()
 	defer conn.Close()
 
 	now := time.Now()
 	seconds := now.Unix()
-	result, err := redis.String(conn.Do("EVALSHA", connPool.ScriptSha, 0, "complete", seconds, jobId, connPool.WorkerName, connPool.Queue, data))
+	result, err := redis.String(conn.Do("EVALSHA", connPool.ScriptSha, 0, "complete", seconds, job.id, connPool.WorkerName, connPool.Queue, job.data))
 	return result, err
 }
 
-func (connPool *QPool) FailJob(jobId string, group string, message string, data string) error {
+func (connPool *QPool) FailJob(job *Job, group string, message string) error {
 	conn := connPool.Pool.Get()
 	defer conn.Close()
 
 	now := time.Now()
 	seconds := now.Unix()
-	_, err := redis.String(conn.Do("EVALSHA", connPool.ScriptSha, 0, "fail", seconds, jobId, connPool.WorkerName, group, message, data))
+	_, err := redis.String(conn.Do("EVALSHA", connPool.ScriptSha, 0, "fail", seconds, job.id, connPool.WorkerName, group, message, job.data))
 	return err
 }
 
